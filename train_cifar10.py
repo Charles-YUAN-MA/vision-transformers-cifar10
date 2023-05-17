726b34feaaef13aa823c66e75483b795199262c0
Yuan Ma <yuma@izar.ib0.cm.izar.local>
2023-05-18 00:04:44 +0200

    5.18

---
+++ train_cifar10.py
@@ -0,0 +1,530 @@
+# -*- coding: utf-8 -*-
+'''
+
+Train CIFAR10 with PyTorch and Vision Transformers!
+written by @kentaroy47, @arutema47
+
+'''
+
+from __future__ import print_function
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+import numpy as np
+
+import torchvision
+import torchvision.transforms as transforms
+
+import os
+import argparse
+import pandas as pd
+import csv
+import time
+
+from models import *
+from utils import progress_bar
+from utils import project_onto_l1_ball
+from randomaug import RandAugment
+from models.vit import ViT
+from models.convmixer import ConvMixer
+
+
+from einops import rearrange, repeat
+from einops.layers.torch import Rearrange
+
+# parsers
+parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser.add_argument('--lr', default=1e-4, type=float, help='learning rate') # resnets.. 1e-3, Vit..1e-4
+#parser.add_argument('--lr', default=1e-3, type=float, help='learning rate')
+parser.add_argument('--opt', default="adam")
+parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
+parser.add_argument('--noaug', action='store_true', help='disable use randomaug')
+parser.add_argument('--noamp', action='store_true', help='disable mixed precision training. for older pytorch versions')
+parser.add_argument('--nowandb', action='store_true', help='disable wandb')
+parser.add_argument('--mixup', action='store_true', help='add mixup augumentations')
+parser.add_argument('--net', default='vit_3_9')
+parser.add_argument('--bs', default='512')
+parser.add_argument('--size', default="32")
+parser.add_argument('--n_epochs', type=int, default='200')
+parser.add_argument('--patch', default='4', type=int, help="patch for ViT")
+parser.add_argument('--dimhead', default="512", type=int)
+parser.add_argument('--convkernel', default='8', type=int, help="parameter for convmixer")
+
+parser.add_argument('--clamp', default=False, type=bool)
+parser.add_argument('--clamp_bottom', default="0", type=float)
+parser.add_argument('--clamp_ceil', default="1", type=float)
+
+parser.add_argument('--l1_constraint', default=True, type=bool)
+parser.add_argument('--q_eps', default="0.0442", type=float)
+parser.add_argument('--k_eps', default="0.0625", type=float)
+
+parser.add_argument('--depth', default="6", type=int)
+
+parser.add_argument('--dataset',default='cifar10',help='cifar10 and cifar100 are supported')
+
+args = parser.parse_args()
+
+# take in args
+usewandb = ~args.nowandb
+if usewandb:
+    import wandb    
+    watermark = "{}_lr{}_{}".format(args.net, args.lr,args.dataset)
+    wandb.init(project="cifar10-challange",
+            name=watermark)
+    wandb.config.update(args)
+
+if args.dataset == 'cifar10':
+    no_classes=10
+elif args.dataset == 'cifar100':
+    no_classes=100
+bs = int(args.bs)
+imsize = int(args.size)
+
+use_amp = bool(~args.noamp)
+aug = args.noaug
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+best_acc = 0  # best test accuracy
+start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+
+# Data
+print('==> Preparing data..')
+if args.net=="vit_timm":
+    size = 384
+else:
+    size = imsize
+
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.Resize(size),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+transform_test = transforms.Compose([
+    transforms.Resize(size),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+# Add RandAugment with N, M(hyperparameter)
+if aug:  
+    N = 2; M = 14;
+    transform_train.transforms.insert(0, RandAugment(N, M))
+
+# Prepare dataset
+if args.dataset == 'cifar10':
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+elif args.dataset == 'cifar100':
+    trainset = torchvision.datasets.CIFAR100(root='./data', train=True, download=True, transform=transform_train)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=bs, shuffle=True, num_workers=8)
+
+if args.dataset == 'cifar10':
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+elif args.dataset == 'cifar100':
+    testset = torchvision.datasets.CIFAR100(root='./data', train=False, download=True, transform=transform_test)
+testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=8)
+
+#classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+# Model factory..
+print('==> Building model..')
+# net = VGG('VGG19')
+if args.net=='res18':
+    net = ResNet18()
+elif args.net=='vgg':
+    net = VGG('VGG19')
+elif args.net=='res34':
+    net = ResNet34()
+elif args.net=='res50':
+    net = ResNet50()
+elif args.net=='res101':
+    net = ResNet101()
+elif args.net=="convmixer":
+    # from paper, accuracy >96%. you can tune the depth and dim to scale accuracy and speed.
+    net = ConvMixer(256, 16, kernel_size=args.convkernel, patch_size=1, n_classes=10)
+elif args.net=="mlpmixer":
+    from models.mlpmixer import MLPMixer
+    net = MLPMixer(
+    image_size = 32,
+    channels = 3,
+    patch_size = args.patch,
+    dim = 512,
+    depth = 6,
+    num_classes = no_classes
+)
+elif args.net=="vit_small":
+    from models.vit_small import ViT
+    net = ViT(
+    image_size = size,
+    patch_size = args.patch,
+    num_classes = no_classes,
+    dim = int(args.dimhead),
+    depth = 6,
+    heads = 8,
+    mlp_dim = 512,
+    dropout = 0.1,
+    emb_dropout = 0.1
+)
+elif args.net=="vit_tiny":
+    from models.vit_small import ViT
+    net = ViT(
+    image_size = size,
+    patch_size = args.patch,
+    num_classes = no_classes,
+    dim = int(args.dimhead),
+    depth = 4,
+    heads = 6,
+    mlp_dim = 256,
+    dropout = 0.1,
+    emb_dropout = 0.1
+)
+elif args.net=="simplevit":
+    from models.simplevit import SimpleViT
+    net = SimpleViT(
+    image_size = size,
+    patch_size = args.patch,
+    num_classes = no_classes,
+    dim = int(args.dimhead),
+    depth = 6,
+    heads = 8,
+    mlp_dim = 512
+)
+elif args.net=="vit":
+    # ViT for cifar10
+    net = ViT(
+    image_size = size,
+    patch_size = args.patch,
+    num_classes = no_classes,
+    dim = int(args.dimhead),
+    depth = args.depth,#from simple
+    heads = 8,
+    mlp_dim = 512,
+    dropout = 0.1,#
+    emb_dropout = 0.1
+)
+elif args.net=="vit_3_9":
+    # ViT for cifar10 modified on March 9
+    from models.vit_3_9 import ViT
+    net = ViT(
+    image_size = size,
+    patch_size = args.patch,
+    num_classes = no_classes,
+    dim = int(args.dimhead),
+    depth = args.depth,
+    heads = 8,
+    mlp_dim = 512,
+    dropout = 0.1,
+    emb_dropout = 0.1
+)
+elif args.net=="vit_origin_depth1":
+    # ViT for cifar10 modified on March 19
+    from models.vit import ViT
+    net = ViT(
+    image_size = size,
+    patch_size = args.patch,
+    num_classes = no_classes,
+    dim = int(args.dimhead),
+    depth = 1,
+    heads = 8,
+    mlp_dim = 512,
+    dropout = 0.1,
+    emb_dropout = 0.1
+)
+
+elif args.net=="vit_3_19":
+    # ViT for cifar10 modified on March 19
+    from models.vit_3_9 import ViT
+    net = ViT(
+    image_size = size,
+    patch_size = args.patch,
+    num_classes = no_classes,
+    dim = int(args.dimhead),
+    depth = 1,
+    heads = 8,
+    mlp_dim = 512,
+    dropout = 0.1,
+    emb_dropout = 0.1
+)
+elif args.net=="vit_3_19_prime":
+    # ViT for cifar10 modified on March 19
+    from models.vit_3_19 import ViT
+    net = ViT(
+    image_size = size,
+    patch_size = args.patch,
+    num_classes = no_classes,
+    dim = int(args.dimhead),
+    depth = 6,
+    heads = 8,
+    mlp_dim = 512,
+    dropout = 0.1,
+    emb_dropout = 0.1
+)
+elif args.net=="vit_timm":
+    import timm
+    net = timm.create_model("vit_base_patch16_384", pretrained=True)
+    net.head = nn.Linear(net.head.in_features, 10)
+elif args.net=="cait":
+    from models.cait import CaiT
+    net = CaiT(
+    image_size = size,
+    patch_size = args.patch,
+    num_classes = no_classes,
+    dim = int(args.dimhead),
+    depth = 6,   # depth of transformer for patch to patch attention only
+    cls_depth=2, # depth of cross attention of CLS tokens to patch
+    heads = 8,
+    mlp_dim = 512,
+    dropout = 0.1,
+    emb_dropout = 0.1,
+    layer_dropout = 0.05
+)
+elif args.net=="cait_small":
+    from models.cait import CaiT
+    net = CaiT(
+    image_size = size,
+    patch_size = args.patch,
+    num_classes = no_classes,
+    dim = int(args.dimhead),
+    depth = 6,   # depth of transformer for patch to patch attention only
+    cls_depth=2, # depth of cross attention of CLS tokens to patch
+    heads = 6,
+    mlp_dim = 256,
+    dropout = 0.1,
+    emb_dropout = 0.1,
+    layer_dropout = 0.05
+)
+elif args.net=="swin":
+    from models.swin import swin_t
+    net = swin_t(window_size=args.patch,
+                num_classes=10,
+                downscaling_factors=(2,2,2,1))
+
+# For Multi-GPU
+'''
+if 'cuda' in device:
+    print(device)
+    print("using data parallel")
+    net = torch.nn.DataParallel(net) # make parallel
+    cudnn.benchmark = True
+'''
+
+if args.resume:
+    # Load checkpoint.
+    print('==> Resuming from checkpoint..')
+    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
+    checkpoint = torch.load('./checkpoint/{}-ckpt.t7'.format(args.net))
+    net.load_state_dict(checkpoint['net'])
+    best_acc = checkpoint['acc']
+    start_epoch = checkpoint['epoch']
+
+# Loss is CE
+criterion = nn.CrossEntropyLoss()
+
+if args.opt == "adam":
+    optimizer = optim.Adam(net.parameters(), lr=args.lr)
+elif args.opt == "sgd":
+    optimizer = optim.SGD(net.parameters(), lr=args.lr)  
+    
+# use cosine scheduling
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, args.n_epochs)
+
+##### Training
+scaler = torch.cuda.amp.GradScaler(enabled=use_amp)
+def train(epoch):
+    print('\nEpoch: %d' % epoch)
+    net.train()
+    #from models.vit import before_info
+    #from models.vit import after_info
+    q_norm =[]
+    k_norm =[]
+    diffnorm = []
+    train_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        inputs, targets = inputs.to(device), targets.to(device)
+        # Train with amp
+        with torch.cuda.amp.autocast(enabled=use_amp):
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+        for i in range(len(net.transformer.layers)):
+            attens, _ = net.transformer.layers[i]
+            #print('before')
+            before = attens.fn.before
+            after = attens.fn.after
+            #print('i',i) 
+            diffnorm.append([i, attens.fn.diffnorm.cpu().detach().numpy()])
+            attens.fn.to_qkv.weight.data.clamp(0,1) 
+            max = attens.fn.to_qkv.weight.data.max().cpu().detach().numpy()
+            min = attens.fn.to_qkv.weight.data.min().cpu().detach().numpy()
+            mean = attens.fn.to_qkv.weight.data.mean().cpu().detach().numpy()        
+
+        if args.clamp:
+            for atten in net.transformer.layers:
+                attens, _ = atten 
+                attens.fn.to_qkv.weight.data = attens.fn.to_qkv.weight.data.clamp(args.clamp_bottom,args.clamp_ceil)
+                
+        if args.l1_constraint:
+            for atten in net.transformer.layers:
+                attens, _ = atten     
+                
+                qkv = attens.fn.to_qkv.weight.chunk(3, dim = 0)
+                '''
+                m = qkv[0].shape
+                print('q.shape:',m)      
+                m = qkv[1].shape
+                print('k.shape:',m)
+                m = qkv[2].shape 
+                print('v.shape:',m)
+                '''
+                q,k,v = qkv
+                #q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = 8), qkv)
+                '''
+                d = q.shape[1]
+                b = q.shape[0]
+                q1 = q.clone()
+                for i in range(b):
+                    #print('q_norm',torch.norm(q[i,:],p=1))
+                    #q_norm.append(torch.norm(q[i,:],p=1))
+                    m = project_onto_l1_ball(q[i,:],args.q_eps)
+                    #print("q_projection_shape",m.shape)
+                    for j in range(d):
+                        q1[i,j] = m[j]
+                #print('k_norm',torch.norm(k,p=1))
+                #k_norm.append(torch.norm(k,p=1))
+                '''
+                print('q_norm',torch.norm(q,p=1))
+
+                q1 = project_onto_l1_ball(q,args.q_eps)
+                #print('q1_norm',torch.norm(q1,p=1))
+                
+                #print('k_norm',torch.norm(k,p=1))
+                k1 = k[0]
+                for i in range(k.shape[0]-1):
+                    k1 = torch.cat((k1,k[i+1]),0)
+                
+                m = project_onto_l1_ball(k1.unsqueeze(0),args.k_eps).squeeze(0).chunk(512,dim=0)
+                k1 = torch.transpose(torch.stack(m,1),0,1)
+                #print('k1_norm',torch.norm(k1,p=1))
+                
+                m = torch.cat((q1,k1,v),dim = 0)
+                attens.fn.to_qkv.weight.data = m
+            
+        optimizer.zero_grad()
+        #print("optimizer.zero_grad()")
+        train_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+
+        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+            % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+    '''        
+    if args.l1_constraint:
+        return train_loss/(batch_idx+1),max,min,mean,before,after,sum(q_norm)/len(q_norm),sum(k_norm)/len(k_norm)
+    else:
+    '''
+    
+    return train_loss/(batch_idx+1),max,min,mean,before,after,diffnorm
+##### Validation
+def test(epoch):
+    global best_acc
+    net.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(testloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+    
+    # Save checkpoint.
+    acc = 100.*correct/total
+    if acc > best_acc:
+        print('Saving..')
+        state = {"model": net.state_dict(),
+              "optimizer": optimizer.state_dict(),
+              "scaler": scaler.state_dict()}
+        if not os.path.isdir('checkpoint'):
+            os.mkdir('checkpoint')
+        torch.save(state, './checkpoint/'+args.net+'-{}-ckpt.t7'.format(args.patch))
+        best_acc = acc
+    
+    os.makedirs("log", exist_ok=True)
+    content = time.ctime() + ' ' + f'Epoch {epoch}, lr: {optimizer.param_groups[0]["lr"]:.7f}, val loss: {test_loss:.5f}, acc: {(acc):.5f}'
+    print(content)
+    with open(f'log/log_{args.net}_patch{args.patch}.txt', 'a') as appender:
+        appender.write(content + "\n")
+    return test_loss, acc
+
+list_loss = []
+list_acc = []
+
+if usewandb:
+    wandb.watch(net)
+net.to('cuda')    
+net.cuda()  
+for epoch in range(start_epoch, args.n_epochs):
+    before_info = []
+    after_info = []
+    start = time.time()
+    '''
+    if args.l1_constraint:
+        trainloss,max,min,mean,before, after, q_norm_mean,k_norm_mean = train(epoch)
+    else:
+    '''
+    trainloss,max,min,mean,before, after,diffnorm = train(epoch)
+    val_loss, acc = test(epoch)
+    
+    scheduler.step(epoch-1) # step cosine scheduling
+    
+    list_loss.append(val_loss)
+    list_acc.append(acc)
+    before_min, before_max, before_mean = before
+    after_min, after_max, after_mean = after    
+    # Log training..
+    
+    if usewandb:
+        if args.net == 'vit':
+            '''
+            if args.l1_constraint:
+                wandb.log({'epoch': epoch, 'train_loss': trainloss, 'val_loss': val_loss, "val_acc": acc, "lr": optimizer.param_groups[0]["lr"],
+        "epoch_time": time.time()-start,"matrix_max":max,"matrix_mean":mean,"matrix_min":min,"before_min":before_min,"before_max":before_max,"before_mean":before_mean,
+        "after_min":after_min,"after_max":after_max,"after_mean":after_mean, "q_norm_mean":q_norm_mean,"k_norm_mean":k_norm_mean})
+            else:
+            '''
+            wandb.log({'epoch': epoch, 'train_loss': trainloss, 'val_loss': val_loss, "val_acc": acc, "lr": optimizer.param_groups[0]["lr"],
+        "epoch_time": time.time()-start,"matrix_max":max,"matrix_mean":mean,"matrix_min":min,"before_min":before_min,"before_max":before_max,"before_mean":before_mean,"after_min":after_min,"after_max":after_max,"after_mean":after_mean})
+	for layer in range(args.depth):
+		wandb.log({f'diffnorm:{layer}':diffnorm[layer][1]})
+        else:
+            wandb.log({'epoch': epoch, 'train_loss': trainloss, 'val_loss': val_loss, "val_acc": acc, "lr": optimizer.param_groups[0]["lr"],
+        "epoch_time": time.time()-start,"matrix_max":max,"matrix_mean":mean,"matrix_min":min,"before_min":before_min,"before_max":before_max,"before_mean":before_mean,"matrix_after_min":after_min,"matrix_after_max":after_max,"matrix_after_mean":after_mean})           
+    # Write out csv..
+    with open(f'log/log_{args.net}_patch{args.patch}.csv', 'w') as f:
+        writer = csv.writer(f, lineterminator='\n')
+        writer.writerow(list_loss) 
+        writer.writerow(list_acc) 
+    print(list_loss)
+
+# writeout wandb
+if usewandb:
+    wandb.save("wandb_{}.h5".format(args.net))
+    
